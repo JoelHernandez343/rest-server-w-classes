0a166a2258d5e7e16657b7b72d350ea2709ac542
JoelHernandez343 <joel.programador@gmail.com>
2021-04-19 15:48:45 -0500

    Init commit

---
+++ src/models/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.PORT = process.env.PORT;
+
+    this.middlewares();
+    this.routes();
+  }
+
+  routes() {
+    this.app.get('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'get API',
+      });
+    });
+
+    this.app.put('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'put API',
+      });
+    });
+
+    this.app.post('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'post API',
+      });
+    });
+
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'delete API',
+      });
+    });
+  }
+
+  middlewares() {
+    this.app.use(express.static('public'));
+  }
+
+  listen() {
+    this.app.listen(this.PORT, () => {
+      console.log(`Listening at port ${this.PORT}`);
+    });
+  }
+}
+
+module.exports = Server;
